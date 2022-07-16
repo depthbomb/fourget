3de55f495d6a68bd92c8fd68f16e7eea82a13e7c
depthbomb <reesepup77@hotmail.com>
2022-07-16 17:22:45 -0500

    chore: update dev dependencies

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ const _os = platform();
 
 export const BINARY_LABEL   = 'fourget' as const;
 export const BINARY_NAME    = 'fourget' as const;
-export const BINARY_VERSION = '0.1.2';
+export const BINARY_VERSION = '0.1.4';
 
 export const IS_WIN32       = _os === 'win32';
 export const IS_LINUX       = ['freebsd', 'openbsd', 'linux'].includes(_os);
