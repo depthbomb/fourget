e49479fabe93b81a15e57f049cecd1edce72201b
depthbomb <reesepup77@hotmail.com>
2022-07-16 21:08:35 -0500

    refactor: simplify entry

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { Cli }             from 'clipanion';
 import { DownloadCommand } from '~commands/download';
 
-async function boot(): Promise<void> {
-	const cli = new Cli({
-		binaryLabel: 'fourget',
-		binaryName: 'fourget',
-		binaryVersion: '0.2.0'
-	});
-	
-	cli.register(DownloadCommand);
-	
-	await cli.runExit(process.argv.slice(2));
-};
+const cli = new Cli({
+	binaryLabel: 'fourget',
+	binaryName: 'fourget',
+	binaryVersion: '0.2.0'
+});
 
-boot();
+cli.register(DownloadCommand);
+cli.runExit(process.argv.slice(2));
